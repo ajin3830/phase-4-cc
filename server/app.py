3e39f736fa8a8e3eb9b7978377b4b840716a48d0
ajin3830 <ajin3830@gmail.com>
2023-03-23 11:58:15 -0400

    2 TypeError but Postman successful

--- server/app.py
+++ server/app.py
@@ -21,10 +21,49 @@ api = Api(app)
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/restaurants')
+# GET /restaurants
+@app.route('/restaurants', methods=['GET'])
 def restaurants():
+    restaurants = Restaurant.query.all()
+    return make_response([restaurant.to_dict() for restaurant in restaurants], 200)
+    
+# GET DELETE /restaurants/:id
+@app.route('/restaurants/<int:id>', methods=['GET', 'DELETE'])
+def restaurant_by_id(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+    if not restaurant:
+        return make_response({"error": "Restaurant not found"}, 404)
+    elif request.method == 'GET':
+        return make_response(restaurant.to_dict(), 200)
+    elif request.method == 'DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response('', 200)
 
-    pass
+# GET /pizzas
+@app.route('/pizzas', methods=['GET'])
+def pizza():
+    pizzas = Pizza.query.all()
+    return make_response([pizza.to_dict() for pizza in pizzas], 200)
+
+# POST /restaurant_pizzas
+@app.route('/restaurant_pizzas', methods=['POST'])
+def restaurant_pizzas():
+    try:
+        new_rp = RestaurantPizza(
+            price=request.get_json()['price'],
+            pizza_id=request.get_json()['pizza_id'],
+            restaurant_id=request.get_json()['restaurant_id']
+        )
+        db.session.add(new_rp)
+        db.session.commit()
+
+        pizza = Pizza.query.filter_by(id=new_rp.pizza_id).first()
+        return make_response(pizza.to_dict(rules=('-created_at', '-updated_at')), 201)
+    
+    except ValueError:
+        return make_response({"errors": ["validation errors"]}, 400)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,4 +10,63 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+                        # many-many plural, join table
+    serialize_rules = ('-restaurants.pizzas', '-restaurant_pizzas')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+    #association
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
+
+    def __repr__(self):
+        return f'<Pizza {self.name} />'
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+                        # many-many plural, join table
+    serialize_rules = ('-pizzas.restaurants', '-restaurant_pizzas')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    #relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref="restaurant")
+    #association
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
+
+    def __repr__(self):
+        return f'<Restaurant {self.name} />'
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+                        # one-many
+    serialize_rules = ('-pizza.restaurants', '-restaurant.pizzas')
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #relationship columns
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    @validates('price')
+    def validate_price(self, key, price):
+        # if not 1 <= price <= 30:
+        if price < 1 or price > 30:
+            raise ValueError('must have a price between 1 and 30')
+        return price
+
+    def __repr__(self):
+        return f'<RestaurantPizza {self.price} />'
+ 
